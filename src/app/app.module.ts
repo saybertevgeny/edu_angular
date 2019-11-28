08d9f205c227539e1cc2506c9064af58747f5485
Evgeny Saybert <saybert@lanit.ru>
2019-11-28 10:58:26 +0300

    two-wat databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -10,9 +9,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
