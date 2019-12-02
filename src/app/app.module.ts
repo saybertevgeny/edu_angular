46131ccbb486ce1882331307287c6b0320bc3852
Evgeny Saybert <saybert@lanit.ru>
2019-12-02 14:44:24 +0300

    init chapter 2, lesson 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostFormComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
