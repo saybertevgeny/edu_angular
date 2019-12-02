3110aa395e5148be7be91315f4e67e838385341a
Evgeny Saybert <saybert@lanit.ru>
2019-12-02 13:35:00 +0300

    pipe 16 lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
-  obj = [
-    {title: 'Post1', author: 'Vladilen', comments: [
-        { name: 'Max', text: 'lorem1'},
-        { name: 'Max', text: 'lorem2'},
-        { name: 'Max', text: 'lorem3'},
-      ]},
-    {title: 'Post2', author: 'Kostya', comments: [
-        { name: 'Petr', text: 'lorem1'},
-        { name: 'Petr', text: 'lorem2'},
-        { name: 'Petr', text: 'lorem3'},
-      ]}
-  ];
+  now: Date = new Date();
 }
