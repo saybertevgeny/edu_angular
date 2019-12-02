eac902fb11f7adae5227a0858e0a9b3720d8e833
Evgeny Saybert <saybert@lanit.ru>
2019-12-02 13:27:43 +0300

    ngFor 15 lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+  obj = [
+    {title: 'Post1', author: 'Vladilen', comments: [
+        { name: 'Max', text: 'lorem1'},
+        { name: 'Max', text: 'lorem2'},
+        { name: 'Max', text: 'lorem3'},
+      ]},
+    {title: 'Post2', author: 'Kostya', comments: [
+        { name: 'Petr', text: 'lorem1'},
+        { name: 'Petr', text: 'lorem2'},
+        { name: 'Petr', text: 'lorem3'},
+      ]}
+  ];
 }
