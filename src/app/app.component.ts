f306a7f3e049d191e2159cf510d5fc2277a77fdd
Evgeny Saybert <saybert@lanit.ru>
2019-12-02 12:13:56 +0300

    event databinding 9 lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,29 @@ export class AppComponent {
       return 'test';
     }
   };
-  img = 'https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/8575147831553750379-128.png';
+  inputValue = '';
+
+  // img = 'https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/8575147831553750379-128.png';
 
   //
 
   constructor() {
-    setTimeout(() => {
-      console.log('Timeout is over');
-      this.img = 'https://angular.io/assets/images/logos/angular/angular.svg';
-    }, 5000);
+    // setTimeout(() => {
+    //   console.log('Timeout is over');
+    //   this.img = 'https://angular.io/assets/images/logos/angular/angular.svg';
+    // }, 5000);
+  }
+
+  onInput(event: KeyboardEvent) {
+    console.log('Event', event);
+    this.inputValue = (<HTMLInputElement> event.target).value;
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str;
+  }
+
+  onClick(event) {
+    console.log('click');
   }
 }
