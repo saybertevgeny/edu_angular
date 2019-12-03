a857867e256ae1af4e4b05624252298d7fc44ea2
Evgeny Saybert <saybert@lanit.ru>
2019-12-03 18:18:16 +0300

    html to content, lesson 6

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
@@ -9,11 +9,12 @@ import {Post} from '../app.component';
 export class PostComponent implements OnInit {
 
   @Input() post: Post;
-
+  @ContentChild('info',{static:true}) infoRef: ElementRef;
   constructor() {
   }
 
   ngOnInit() {
+    console.log(this.infoRef.nativeElement);
   }
 
 }
