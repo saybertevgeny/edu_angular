4804db634c1ce54f4e42ca0a661bddfde19fb0fc
Evgeny Saybert <saybert@lanit.ru>
2019-12-03 17:05:29 +0300

    pass params to template, lesson 2

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from '../app.component';
 
 @Component({
   selector: 'app-post',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  //@Input('myPost') post: Post
+  @Input() post: Post
   constructor() { }
 
   ngOnInit() {
