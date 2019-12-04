1bf17b7e78e7920545f85b5ca4f8b75476c0c3d6
Evgeny Saybert <saybert@lanit.ru>
2019-12-04 18:14:35 +0300

    using generic type, lesson 6

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
-interface SayHello {
-    sayHello: () => void
+const arr: Array<number> = [1, 2, 3, 4];
+
+interface User {
+    id: number
+    name: string
+    age: number
 }
 
-class User implements SayHello{
-    constructor(protected name: string, private age: number) {}
+const users: Array<User> = [
+    {id: 1, name: 'V', age: 25},
+    {id: 2, name: 'E', age: 28}
+];
 
-    sayHello(): void{
-        console.log(this.name + ' hello!');
-    }
-
-}
+const users2: User[] = [
+    {id: 1, name: 'V', age: 25},
+    {id: 2, name: 'E', age: 28}
+];
