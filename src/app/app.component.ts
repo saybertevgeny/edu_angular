4a09b4bcdf781f9395d6e620f82f415ff85dfde0
Evgeny Saybert <saybert@lanit.ru>
2019-12-04 12:23:02 +0300

    ChangeDetectionStrategy onPush, lesson 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 export interface Post {
   title: string
@@ -11,11 +11,22 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   posts: Post[] = [
     {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
     {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2}
   ];
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('Timeout');
+      this.posts[0] = {
+        title: 'Changed',
+        text: 'changed 2',
+        id: 33
+      };
+    },5000)
+  }
 
   updatePosts(post: Post) {
     this.posts.unshift(post);
