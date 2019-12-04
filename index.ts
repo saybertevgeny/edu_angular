8b96f5947b5b1c720d4e4165572574d6669b07fd
Evgeny Saybert <saybert@lanit.ru>
2019-12-04 18:17:45 +0300

    Creating generic types

--- index.ts
+++ index.ts
@@ -1,17 +1,8 @@
-const arr: Array<number> = [1, 2, 3, 4];
-
-interface User {
-    id: number
-    name: string
-    age: number
+class User<T,P>{
+    private name:T;
+    private password:P;
 }
 
-const users: Array<User> = [
-    {id: 1, name: 'V', age: 25},
-    {id: 2, name: 'E', age: 28}
-];
-
-const users2: User[] = [
-    {id: 1, name: 'V', age: 25},
-    {id: 2, name: 'E', age: 28}
-];
+interface Interface<T> {
+    name:T;
+}
