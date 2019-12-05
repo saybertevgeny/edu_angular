175fe210e15fcb3d1079d2ec39a98e77cb6e618a
Evgeny Saybert <saybert@lanit.ru>
2019-12-05 15:24:17 +0300

    relation between html form's input and FormControl object,lesson 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,25 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   form: FormGroup;
 
   ngOnInit(): void {
-    this.form = new FormGroup({});
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null),
+    });
   }
 
   submit() {
-    console.log('Form submitted: ',this.form);
+    console.log('Form:', this.form);
+    const formData = {...this.form.value};
+    console.log('Form data: ',formData)
   }
 }
 
