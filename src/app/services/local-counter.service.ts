bbcaa37f45692f07a0391524e28046d8aa0d200d
Evgeny Saybert <saybert@lanit.ru>
2019-12-05 14:23:07 +0300

    di between services di,lesson 5

--- src/app/services/local-counter.service.ts
+++ src/app/services/local-counter.service.ts
@@ -1,14 +1,19 @@
 import {Injectable} from '@angular/core';
+import {LogService} from './log.service';
 //created command ng g s services/local-counter
 @Injectable()
 export class LocalCounterService {
   counter: number = 0;
 
+  constructor(private logService:LogService){}
+
   increase() {
+    this.logService.log('increase in local')
     this.counter++;
   }
 
   decrease() {
+    this.logService.log('decrease in local')
     this.counter--;
   }
 }
