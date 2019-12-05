51814654f7ac5c188a1c8c3b06ce2a7981b60e93
Evgeny Saybert <saybert@lanit.ru>
2019-12-06 02:08:55 +0300

    creating http interceptor,lesson 12

---
+++ src/app/auth.interceptor.ts
@@ -0,0 +1,11 @@
+import {HttpHandler, HttpInterceptor, HttpRequest, HttpEvent} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export class AuthInterceptor implements HttpInterceptor{
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Intercept request',req);
+    return next.handle(req);
+  }
+
+}
