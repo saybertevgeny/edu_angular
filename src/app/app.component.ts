22954f373234c8e0b1696ac9e77d7bdf3470b72d
Evgeny Saybert <saybert@lanit.ru>
2019-12-05 16:44:50 +0300

    reset form,lesson 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,7 @@ export class AppComponent implements OnInit {
       console.log('Form:', this.form);
       const formData = {...this.form.value};
       console.log('Form data: ', formData);
+      this.form.reset();
     }
   }
 
