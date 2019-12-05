454dbaeb7e5904ab76df3c585580cd847388bb8c
Evgeny Saybert <saybert@lanit.ru>
2019-12-06 02:14:13 +0300

    using http interceptor,lesson 13

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,11 +1,22 @@
-import {HttpHandler, HttpInterceptor, HttpRequest, HttpEvent} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpHandler, HttpInterceptor, HttpRequest, HttpEvent, HttpEventType} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
-export class AuthInterceptor implements HttpInterceptor{
+export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Intercept request',req);
-    return next.handle(req);
+    console.log('Intercept request', req);
+
+    const cloned = req.clone({//если модифицировать существующий request все перестанет работать
+      headers: req.headers.append('Auth', 'some render tocken')
+    });
+    return next.handle(cloned).pipe(
+      tap(event => {
+        if (event.type == HttpEventType.Response) {
+          console.log('Interceptor response', event);
+        }
+      })
+    );
   }
 
 }
