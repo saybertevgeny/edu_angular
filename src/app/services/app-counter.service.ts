ee7622b430ca3850c4470a23e46c320c6f82f033
Evgeny Saybert <saybert@lanit.ru>
2019-12-05 14:21:14 +0300

    di between services di,lesson 5

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,17 +1,23 @@
 import {Injectable} from '@angular/core';
+import {LogService} from './log.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppCounterService {
 
+  constructor(private logService: LogService) {
+  }
+
   counter: number = 0;
 
   increase() {
+    this.logService.log('increase counter');
     this.counter++;
   }
 
   decrease() {
+    this.logService.log('decrease counter');
     this.counter--;
   }
 }
