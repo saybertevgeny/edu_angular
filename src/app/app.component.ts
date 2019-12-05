42a033283b9320970b5e7c9ca148358862820543
Evgeny Saybert <saybert@lanit.ru>
2019-12-06 00:04:59 +0300

    getting request and its output ,lesson 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+export interface Todo {
+  completed: boolean,
+  title: string,
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -7,8 +14,17 @@ import {Component, OnInit} from '@angular/core'
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+  todos: Todo[] = [];
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .subscribe(todos => {
+        console.log('Response: ',todos);
+        this.todos = todos;
+      });
   }
 }
 
