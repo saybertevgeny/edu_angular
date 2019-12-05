b9aea50c84d0e0850bf20aa2e8ed13e3042205b4
Evgeny Saybert <saybert@lanit.ru>
2019-12-05 16:01:09 +0300

    update formControl value from event ,lesson 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,33 @@ export class AppComponent implements OnInit {
     this.form = new FormGroup({
       email: new FormControl('',
         [Validators.email, Validators.required]),
-      password: new FormControl(null,[Validators.required,Validators.minLength(6)]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
       address: new FormGroup({
         country: new FormControl('by'),
-        city: new FormControl('',Validators.required)
+        city: new FormControl('', Validators.required)
       })
     });
-  }``
+  }
 
   submit() {
-    if(this.form.valid) {
+    if (this.form.valid) {
       console.log('Form:', this.form);
       const formData = {...this.form.value};
       console.log('Form data: ', formData);
     }
+  }
+
+  setCapital() {
+    const cityMap = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск'
+    };
+    const cityKey:string  = this.form.get('address').get('country').value;
+    const city = cityMap[cityKey];
+    this.form.patchValue({
+      address:{city}//равносильно такому address:{city:city}
+    });
   }
 }
 
