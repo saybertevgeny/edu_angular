2bc6937965cbcb0a5ab2d9006da78904438ba4a9
Evgeny Saybert <saybert@lanit.ru>
2019-12-09 13:15:55 +0300

    preparing context in dependence query params,lesson 7

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,24 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Post, PostsService} from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
+  private post: Post;
+  constructor(private route:ActivatedRoute, private postsService: PostsService,private router:Router) {}
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.post = this.postsService.getById(+params.id);
+    })
+  }
+
+
+  loadPost() {
+    this.router.navigate(['/posts',44])
+  }
 }
