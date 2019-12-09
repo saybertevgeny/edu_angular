19f02ca0a3649c2c7643699ef2d1512ff5c29e83
Evgeny Saybert <saybert@lanit.ru>
2019-12-09 13:29:57 +0300

    pass query params and fragment to url,lesson 8

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,33 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
 import {PostsService} from '../posts.service'
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  constructor(private postsService: PostsService) {}
+export class PostsComponent implements OnInit{
+  showIds: boolean = false;
+
+  constructor(private postsService: PostsService,private route:ActivatedRoute,private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.showIds = !!params.showIds;
+    })
+    this.route.fragment.subscribe((fragment:string) => {
+      console.log('Fragment:',fragment);
+    })
+  }
+
+
+  showIdsProgram() {
+    this.router.navigate(['/posts'],{
+      queryParams: {
+        showIds: true
+      },
+      fragment: 'program-fragment'
+    })
+  }
 }
