41fb1eedf329233b35e12a254db3f81eecfbaba0
Evgeny Saybert <saybert@lanit.ru>
2019-12-09 14:16:39 +0300

    authenticate ,lesson 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,16 @@ import {PostsComponent} from './posts/posts.component';
 import {PostComponent} from './post/post.component';
 import {AboutExtraComponent} from './about-extra/about-extra.component';
 import {ErrorPageComponent} from './error-page/error-page.component';
+import {AuthGuard} from './auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent, children: [
-      {path:'extra', component: AboutExtraComponent}
-    ]},
-  {path: 'posts', component: PostsComponent},
+  {
+    path: 'about', component: AboutComponent, children: [
+      {path: 'extra', component: AboutExtraComponent}
+    ]
+  },
+  {path: 'posts', component: PostsComponent, canActivate: [AuthGuard]},
   {path: 'posts/:id', component: PostComponent},
   {path: 'error', component: ErrorPageComponent},
   {path: '**', redirectTo: 'error'} //Всегда последний!
